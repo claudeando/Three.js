add53fbbb1e505e9c44897efd264bcdd19e9254a
claudeando <claude.mm6@gmail.com>
2022-11-08 10:36:47 +0000

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import gsap from 'gsap'
 
 const canvas = document.createElement('canvas')
 canvas.width = 650
@@ -13,18 +14,19 @@ window.addEventListener('load', init)
 function init() {
 
   // basic scene setup
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(50, canvas.width / canvas.height, 0.1, 100)
+  camera.position.z = 5
+
   const renderer = new THREE.WebGLRenderer()
   renderer.setSize(canvas.width, canvas.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
   renderer.shadowMap.enabled = true
   document.body.append(renderer.domElement)
 
-  const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(50, canvas.width / canvas.height, 0.1, 100)
-  camera.position.z = 5
-
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.enableDamping = true
+  controls.dampingFactor = 0.125
   controls.update()
 
 
@@ -74,19 +76,9 @@ function init() {
   })
   const particles = new THREE.Points(particleGeo, particleMat)
 
-  scene.add(particles)
+  // scene.add(particles)
 
-
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshNormalMaterial()
-  )
-
-  // const shaderGeo = new THREE.BoxBufferGeometry(1, 1, 1, 32, 32, 32)
-  // const shaderGeo = new THREE.SphereBufferGeometry(1, 64, 64)
-  // const shaderGeo = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
-  // const shaderGeo = new THREE.CircleBufferGeometry(1, 180)
-  const shaderGeo = new THREE.RingGeometry(1, 2, 532);
+  const shaderGeo = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
   const shaderMat = new THREE.PointsMaterial({
     color: new THREE.Color('white'),
     size: 0.2,
@@ -94,7 +86,58 @@ function init() {
   })
   const shader = new THREE.Points(shaderGeo, shaderMat)
 
-  scene.add(mesh)
+
+
+  const geo = new THREE.BoxBufferGeometry(1, 1, 1, 16, 16, 16)
+  const mat = new THREE.MeshNormalMaterial({ wireframe: true })
+  for (let i = 0; i < 100; i++) {
+    const mesh = new THREE.Mesh(geo, mat)
+    mesh.position.x = (Math.random() - 0.5) * 20
+    mesh.position.y = (Math.random() - 0.5) * 20
+    mesh.position.z = (Math.random() - 0.5) * 20
+    mesh.rotation.x = (Math.random() - 0.5)
+    mesh.rotation.z = (Math.random() - 0.5)
+
+    scene.add(mesh)
+  }
+
+
+  // loading page
+  const loadingManager = new THREE.LoadingManager(
+    // loaded
+    () => {
+      console.log('hello')
+      gsap.to(loadingMat.uniforms.uAlpha, { duration: 3, value: 0 })
+    },
+    // progress
+    () => {
+      console.log('loading in progress')
+    }
+  )
+
+  const loadingGeo = new THREE.PlaneBufferGeometry(2, 2)
+  const loadingMat = new THREE.ShaderMaterial({
+    uniforms: {
+      uAlpha: { value: 1 }
+    },
+    vertexShader: `
+    void main() {
+      gl_Position = vec4(position, 1.0);
+    }
+    `,
+    fragmentShader: `
+    uniform float uAlpha;
+
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 0.0, uAlpha);
+    }
+    `,
+    transparent: true
+  })
+  const loading = new THREE.Mesh(loadingGeo, loadingMat)
+  loading.rotation.x = -Math.PI / 2
+  scene.add(loading)
+
 
 
 
@@ -116,7 +159,7 @@ function init() {
   const animate = () => {
     const elapsedTime = clock.getElapsedTime()
 
-    particles.rotation.x += 0.001
+    // particles.rotation.x = Math.random() * 0.02
 
     controls.update()
 
